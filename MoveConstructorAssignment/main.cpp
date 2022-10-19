9b4b8802e9eab9451fd59ee7aac37b12c7a13062
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-18 21:13:28 -0400

    Initial Commit

---
+++ MoveConstructorAssignment/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MoveConstructorAssignment
+//
+//  Created by Matt Ahmad on 2022-10-18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
