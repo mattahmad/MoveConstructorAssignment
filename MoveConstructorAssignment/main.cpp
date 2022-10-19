d0e4d139d98b2f5e2c5a809d354e251749f67fe8
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-18 21:28:23 -0400

    Move semantics

--- MoveConstructorAssignment/main.cpp
+++ MoveConstructorAssignment/main.cpp
@@ -1,14 +1,32 @@
-//
-//  main.cpp
-//  MoveConstructorAssignment
-//
-//  Created by Matt Ahmad on 2022-10-18.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Example {
+        string* ptr;
+    public:
+    Example (const string& str) : ptr(new string(str)) {}
+    ~Example () {delete ptr;}
+    Example (Example&& x) : ptr(x.ptr) {x.ptr=nullptr;}
+    Example& operator= (Example&& x) {
+        delete ptr;
+        ptr = x.ptr;
+        x.ptr=nullptr;
+        return *this;
+    }
+    const string& content() const {return *ptr;}
+    Example operator+(const Example& rhs){
+        return Example(content()+rhs.content());
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    Example foo ("Exam");
+    Example bar = Example("ple");
+    
+    foo = foo + bar;
+    
+    cout << "foo's content: " << foo.content() << '\n';
     return 0;
 }
